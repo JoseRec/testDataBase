ccd76f8789da50ac2978c3897bc6aade77857334
JoseRec <joseandresrc6@gmail.com>
2024-12-02 18:04:17 -0600

    Nueva Modificacion

--- DataBase/src/database/clase.java
+++ DataBase/src/database/clase.java
@@ -7,4 +7,9 @@ public class clase {
     public void saludar(){
         JOptionPane.showMessageDialog(null, "Hola! \nComo estas?");
     }
+    
+    public void ingName(){
+        String name = JOptionPane.showInputDialog("Ingrese su nombre: ");
+        JOptionPane.showMessageDialog(null, "Tu nombre es: " + name );
+    }
 }
